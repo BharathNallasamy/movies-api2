5957bff73616055d87aff7d2efa1927e74ac1d33
bharath2023ht <126450752+bharath2023ht@users.noreply.github.com>
2024-07-06 20:51:32 +0530

    'update'

--- main.js
+++ main.js
@@ -9,6 +9,27 @@ app.get("/",(req, res) => {
   res.json({msg: "Create Method"})
 })
 
+// CRUD functionalities of Movies
+// R - Reading
+app.get('/movies', (req, res) => {
+  
+})
+
+// C - Create
+app.post('/movies', (req, res) => {
+  
+})
+
+// U - Update
+app.put('/movies/:id', (req, res) => {
+  
+})
+
+// D - Delete
+app.delete('/movies/:id', (req, res) => {
+  
+})
+
 app.listen(PORT, () => {
   console.log(`The Port is Running at: ${PORT}`);
 })
